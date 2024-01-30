cacf547a59e267eca1b457d89e82e1b8399b9c5d
Anand.C <anandchannazhi01@gmail.com>
2024-01-30 15:55:45 +0530

    Implement 'Enter' key functionality to insert line breaks in document contenteditable area.

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+showNotes();
+
 function updateStorage(){
     localStorage.setItem("notes",notesContainer.innerHTML);
 }
@@ -26,5 +31,13 @@ notesContainer.addEventListener("click", function(e){
                 updateStorage();
             }
         })
+    }
+})
+
+
+document.addEventListener("keydown", event =>{
+    if(event.key === "Enter" ){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
 })
